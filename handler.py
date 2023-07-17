7b3addd2dbb4a6d3353d07338799f890d7bd5974
Ghassen-Da <ghassendaoud99@gmail.com>
2023-07-17 18:37:22 -0400

    commit

--- handler.py
+++ handler.py
@@ -25,18 +25,19 @@ encoded_jwt = jwt.encode(payload, private_key, algorithm="RS256")
 
 def handler(event, context):
   data=json.loads(event['body'])
-  print('data:', data)
+  print('data:', data['base'])
   if data['action']=='opened':
      owner=data['pull_request']['head']['repo']['owner']['login']
-     repo_name=data['pull_request']['head']['repo']['name'] 
+     repo_name=data['pull_request']['head']['repo']['name']
+     branch_name=data['base']['ref']
      pr_number=data['number']
-  token=clone_repo(owner, repo_name)
+  token=clone_repo(owner, repo_name, branch_name)
   neura_checker.check_repo(owner, repo_name, token, pr_number)
   return "return"
 
 
 # Repo full name is owner_name/repo_name
-def clone_repo(owner, repo_name):
+def clone_repo(owner, repo_name, branch_name):
   # Get the installation ID
   url_to_get_installation_id=f'https://api.github.com/repos/{owner}/{repo_name}/installation'
   headers = {'Authorization': f'Bearer {encoded_jwt}', 'X-GitHub-Api-Version':'2022-11-28', 'Accept':'application/vnd.github+json'}
@@ -51,5 +52,5 @@ def clone_repo(owner, repo_name):
   clone_url = f"https://x-access-token:{token}@github.com/{owner}/{repo_name}.git"
   # Use the clone URL to clone the repository with Git
   os.chdir('/tmp')
-  os.system(f"git clone {clone_url}")
+  os.system(f"git clone -b {branch_name} {clone_url}")
   return token
