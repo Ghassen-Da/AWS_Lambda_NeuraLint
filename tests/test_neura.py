ca506c89cc7868ffb77d650e822d1364bf72c128
Ghassen-Da <ghassendaoud99@gmail.com>
2023-07-18 14:55:19 -0400

    add ci

--- tests/test_neura.py
+++ tests/test_neura.py
@@ -1,21 +1,16 @@
 import unittest
 import os
-print(os.getcwd())
 os.chdir("tests")
-print(os.getcwd())
 import sys
 sys.path.append('..')
 from endToEnd import main
-# import shutil
 
-# shutil.copyfile(src='tests/', dst='tests/endToEnd.py')
-
-# from ..endToEnd import main
-
-class NeuraSum(unittest.TestCase):
-    def test_neura():
+class NeuraTest(unittest.TestCase):
+    def test_neura(self):
         print('Start testing...')
         output = main("mix_bugs_1", [32,28,28,1], [32,10], "tf", "result")
         print(output)
         print('Testing completed.')
 
+if __name__ == '__main__':
+    unittest.main()
