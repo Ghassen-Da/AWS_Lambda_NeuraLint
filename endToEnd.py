a64f6eaaf63753e1c950d2d7249cd8f8aaf54547
Ghassen-Da <ghassendaoud99@gmail.com>
2023-07-21 13:20:43 -0400

    commit

--- endToEnd.py
+++ endToEnd.py
@@ -53,7 +53,7 @@ def parsDnnScript(fileName, parser = "tf" , inputSize=[128, 20], outputSize=[128
             parser_TF.main(fileName, input_size= inputSize, output_size=outputSize)
             print("Running Model Checker (Groove) ... \n")
             if os.environ.get("TEST_MODE")=="1":
-                print('here')
+                print('here', os.listdir(f'../'))
                 with open(f'..{grammarName}.gps','r') as f:
                     print('1')
                     print(f.read())
