7ad77536a7fc45a20124a67fca5e4688b2c4d980
Ghassen-Da <ghassendaoud99@gmail.com>
2023-07-24 13:39:52 -0400

    commit

--- Python Library/grooveParser.py
+++ Python Library/grooveParser.py
@@ -183,7 +183,7 @@ def main(inputGraphName="TF_lenet", parserType = "tf"):
         BuggyLayerName = item.get("from")
         buggyLayerNumber = nodeName.get(BuggyLayerName)
         layerErrors = flags.get(item.get("to"))
-        report = "Layer {} ==> ".format(buggyLayerNumber)
+        report = "Layerrrrrr {} ==> ".format(buggyLayerNumber)
         for item in layerErrors:
             report += faultCodeTranslator(item)
             if layerErrors.index(item) != len(layerErrors)-1:
@@ -197,7 +197,7 @@ def main(inputGraphName="TF_lenet", parserType = "tf"):
         for item in sortedBuggyLayers:
             output += sortedBuggyLayers.get(item)
             if item != lastKey:
-                output += "\n\r*****************"
+                output += "\n\r"
 
     if not buggyFlag:
         output = "There is no identified fault in the DNN script"
